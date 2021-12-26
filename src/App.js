2d43fe8aec1caea86b99e4d316e775155a1c4a2e
SaySayo <sayobamigbade@gmail.com>
2021-12-26 12:05:49 +0100

    Revert "main page"
    This reverts commit e615d7134fe2c47b880b3cc0ed1fee838e23a612.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import {Home} from './pages/Home';
-
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
