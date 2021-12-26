e615d7134fe2c47b880b3cc0ed1fee838e23a612
SaySayo <sayobamigbade@gmail.com>
2021-12-26 09:54:18 +0100

    main page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+export const Home = () => {
+  return <div className="body">
+      <div className="rect-1"> 
+      <div className="apple-logo" />
+      </div>
+      <div className="container">
+          <div className="rect-2">
+              <div className="rect-3">
+                  <div className="rect-4"></div>
+                  <div className="rect-5"></div>
+                  <div className="rect-6"></div>
+              </div>
+              <div className="rect-7"></div>
+              <div className="rect-8"></div>
+          </div>
+          <div className="rect-9"></div>
+          <div className="rect-10"></div>
+          <div className="rect-11">
+                <div className="rect-12"></div>
+          </div>
+      </div>
+      <div className="rect-13"></div>
+      <div className="rect-14"></div>
+      <div className="rect-15"></div>
+  </div>;
+};
